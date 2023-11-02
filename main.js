f72bae4f8115065550ad8712073786b61f46e07f
Edward Kirakosyan <kirakosyan3010@gmail.com>
2023-11-02 15:55:42 +0400

    c\

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+import { createClient } from "@supabase/supabase-js"
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase"
+import { OpenAIEmbeddings } from "langchain/embeddings/openai"
 
 try {
-  const result = await fetch("scrimba-info.txt")
+  const result = await fetch("info.txt")
   const text = await result.text()
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 500,
@@ -10,6 +13,21 @@ try {
   })
 
   const output = await splitter.createDocuments([text])
+
+  const sbApiKey = process.env.SUPABASE_KEY
+  const sbUrl = supabase
+  const openAIApiKey = process.env.SUPABASE_KEY
+
+  const client = createClient(sbUrl, sbApiKey)
+
+  await SupabaseVectorStore.fromDocuments(
+    output,
+    new OpenAIEmbeddings({ openAIApiKey }),
+    {
+      client,
+      tableName: "documents",
+    }
+  )
 } catch (err) {
   console.log(err)
 }
